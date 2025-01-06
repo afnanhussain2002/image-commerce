5998f522ad959ae7a80265552414831e3e5e4866
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-06 12:13:31 +0600

    add a .then() on that cached func

--- lib/db.ts
+++ lib/db.ts
@@ -3,27 +3,26 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-    throw new Error("Check your database connection string");
+  throw new Error("Check your database connection string");
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export async function connectToDatabase(){
-    if(cached.conn){
-        return cached.conn;
-    }
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if(!cached.promise){
-        const opts = {
-            bufferCommands: true,
-            maxPoolSize: 10,
-        }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
 
-        cached.promise = mongoose.connect(MONGODB_URI, opts)
-    }
-
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then();
+  }
 }
