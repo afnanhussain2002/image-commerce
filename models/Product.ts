61350c3f60b03ef736159932019f0b97909891ae
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-07 11:07:31 +0600

    write the schema of product

---
+++ models/Product.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const imageVariantSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ["SQUARE", "WIDE", "PORTRAIT"],
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  license: {
+    type: String,
+    required: true,
+    enum: ["personal", "commercial"],
+  },
+});
