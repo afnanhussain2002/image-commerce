9c5f3a11694ec879b8ec9f30cce2b80587edbeb8
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-07 13:59:11 +0600

    complete the order schema

--- models/Order.ts
+++ models/Order.ts
@@ -1,5 +1,30 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 const orderSchema = new Schema({
-    
+   userId:{
+    type:Schema.Types.ObjectId,
+    required:true,
+    ref:"User"
+   },
+   productId:{
+    type:Schema.Types.ObjectId,
+    required:true,
+    ref:"Product"
+   },
+   variant:{
+    type:{
+        type:String,
+        required:true,
+        enum:["SQUARE", "WIDE", "PORTRAIT"]
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    license:{
+        type:String,
+        required:true,
+        enum:["personal","commercial"]
+    }
+   }
 })
