9f7e61f1bbed4747c9b025aa7ee99d4cc91481c1
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-09 16:52:17 +0600

    define that credentials

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,12 @@ import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions ={
     providers: [
-
+        CredentialsProvider({
+            name: "Credentials",
+            credentials:{},
+            async authorize(credentials) {
+               return {id: 1, name:'admin', role: 'admin'}
+            }
+        })
     ]
 }
