e43c78e12b862a4432335b101b6e301c75303a30
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-09 16:55:10 +0600

    write email and password as a credentials

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,18 @@ export const authOptions: NextAuthOptions ={
     providers: [
         CredentialsProvider({
             name: "Credentials",
-            credentials:{},
+            credentials:{
+                email:{
+                    label: "Email",
+                    type: "email",
+                    placeholder: "email"
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "password"
+                }
+            },
             async authorize(credentials) {
                return {id: 1, name:'admin', role: 'admin'}
             }
