8d63a83db5664986f652cb86417020c1f8e93d9b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-13 21:26:28 +0600

    write the session part

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,55 +3,64 @@ import { connectToDatabase } from "./db";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
-export const authOptions: NextAuthOptions ={
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials:{
-                email:{
-                    label: "Email",
-                    type: "email",
-                    placeholder: "email"
-                },
-                password: {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "password"
-                }
-            },
-            async authorize(credentials) {
-               if (!credentials?.email || !credentials?.password) {
-                throw new Error("Invalid credentials");
-               }
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
 
-               try {
-                await connectToDatabase();
+        try {
+          await connectToDatabase();
 
-                const user = await User.findOne({email: credentials.email});
+          const user = await User.findOne({ email: credentials.email });
 
-                if (!user) {
-                    throw new Error("No User Found with this email");
-                }
+          if (!user) {
+            throw new Error("No User Found with this email");
+          }
 
-                const isValid = await bcrypt.compare(credentials.password, user.password);
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
 
-                if (!isValid) {
-                    throw new Error("Invalid Password");
-                }
+          if (!isValid) {
+            throw new Error("Invalid Password");
+          }
 
-                return {
-                    id: user._id.toString(),
-                    email: user.email,
-                    role: user.role
-                }
-               } catch (error) {
-                console.error("Auth error",error);
-                throw error;
-               }
-            }
-        })
-    ],
-    callbacks:{
-        
-    }
-}
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            role: user.role,
+          };
+        } catch (error) {
+          console.error("Auth error", error);
+          throw error;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+
+      return session;
+    },
+  },
+};
