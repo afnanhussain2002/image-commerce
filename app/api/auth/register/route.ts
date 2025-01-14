e5ffa9c16414d98b222ccb4a0af410664b20fbb8
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-14 11:42:43 +0600

    create the user and return a response

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -3,21 +3,31 @@ import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    try {
-        const { email, password } = await request.json();
+  try {
+    const { email, password } = await request.json();
 
-        if (!email || !password) {
-            return NextResponse.json({ error: "email and password required" }, { status: 400 });
-        }
-
-        await connectToDatabase();
-
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            return NextResponse.json({ error: "Email is already in use" }, { status: 400 });
-        }
-    } catch (error) {
-        console.log(error);
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "email and password required" },
+        { status: 400 }
+      );
     }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email is already in use" },
+        { status: 400 }
+      );
+    }
+
+    await User.create({ email, password, role: "user" });
+
+    return NextResponse.json({ message: "User registered successfully" },{status:201});
+  } catch (error) {
+    console.log(error);
+  }
 }
