e7b4243829a7792d01995a56abaa2265de431385
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-14 11:35:51 +0600

    get the email and password

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,6 +2,8 @@ import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
     try {
+        const { email, password } = await request.json();
+
         
     } catch (error) {
         
