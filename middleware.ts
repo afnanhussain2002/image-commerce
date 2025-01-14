2631b238cfc9fc6c333394797544be8b50c0d6aa
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-14 11:53:23 +0600

    add req on auth callback

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,9 @@ export default withAuth(
   },
   {
     callbacks: {
-      authorized: ({ token }) => token?.role === "admin",
+      authorized: ({ token, req }) =>{
+           
+      } 
     },
   },
 )
