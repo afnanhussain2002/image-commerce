fc190ba364069d51e8af1eaa46f15e7f8cc55ed4
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-15 15:19:58 +0600

    make a provider func and export it

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -4,3 +4,7 @@ import { ImageKitProvider, IKImage } from "imagekitio-next";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+ 
+}
