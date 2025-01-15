7baf7a5423a6f97b65e06896cb87708349975855
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-15 16:47:46 +0600

    return imagekit provider

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -6,18 +6,29 @@ const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-     const authenticator = async () => {
-         try {
-            const response = await fetch("/api/imagekit-auth");
-            if (!response.ok) {
-                const errorText = await response.text();
-                throw new Error(`Request failed with status ${response.status}: ${errorText}`);
-              }
-              return response.json();
-         } catch (error) {
-             console.error("imagekit auth error", error);
-             throw error;
-            
-         }
-     }
+  const authenticator = async () => {
+    try {
+      const response = await fetch("/api/imagekit-auth");
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Request failed with status ${response.status}: ${errorText}`
+        );
+      }
+      return response.json();
+    } catch (error) {
+      console.error("imagekit auth error", error);
+      throw error;
+    }
+  };
+
+  return (
+    <ImageKitProvider
+      urlEndpoint={urlEndpoint}
+      publicKey={publicKey}
+      authenticator={authenticator}
+    >
+      {children}
+    </ImageKitProvider>
+  );
 }
