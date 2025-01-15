62ea209d1b235227f3d6288a70495540c8b73117
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-15 16:48:54 +0600

    wrap it with session provider

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { ImageKitProvider, IKImage } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
@@ -23,12 +24,14 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <ImageKitProvider
+  <SessionProvider>
+      <ImageKitProvider
       urlEndpoint={urlEndpoint}
       publicKey={publicKey}
       authenticator={authenticator}
     >
       {children}
     </ImageKitProvider>
+  </SessionProvider>
   );
 }
