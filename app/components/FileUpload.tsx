1ba8bf2cca4f5496db6e1089eb0b86ca9482127e
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 18:32:00 +0600

    add a state for upload

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -4,6 +4,9 @@ import { IKUpload } from "imagekitio-next";
 import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
 
 export default function FileUpload() {
+
+    const [uploading, setUploading] = useState(false);
+    
     return (
         <div>FileUpload</div>
     )
