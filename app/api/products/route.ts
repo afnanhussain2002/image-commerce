29b613bafc886f87195101c48ebc0ed3e1f2eca1
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 20:29:03 +0600

    send a message if no product found

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,10 +1,15 @@
 import { connectToDatabase } from "@/lib/db";
 import Product from "@/models/Product";
+import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
         await connectToDatabase();
         const products = await Product.find({}).lean();
+
+        if (!products || products.length === 0) {
+            return NextResponse.json({ message: "No products found" }, { status: 404 });
+        }
     } catch (error) {
         
     }
