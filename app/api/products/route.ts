7df575d2876f25598694a3031dd201ee56aaef02
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 20:27:57 +0600

    try to get the product

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,8 +1,10 @@
 import { connectToDatabase } from "@/lib/db";
+import Product from "@/models/Product";
 
 export async function GET() {
     try {
         await connectToDatabase();
+        const products = await Product.find({}).lean();
     } catch (error) {
         
     }
