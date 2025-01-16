9a45e20ec57af4b79c35e858d47bf82fe4c6bf66
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 18:28:54 +0600

    import the response of ik upload

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { IKUpload } from "imagekitio-next";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
 
 export default function FileUpload() {
     return (
