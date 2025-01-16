84cf5724e34c583356c3a7bcee88d6470a96d060
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 20:32:11 +0600

    post a product code start

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -17,3 +17,7 @@ export async function GET() {
         return NextResponse.json({ error: "Failed to fetch products" }, { status: 500 });
     }
 }
+
+export async function POST(request: Request) {
+    
+}
