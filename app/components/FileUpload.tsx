7642a0783c58cda32ecea702dd8793333596212b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-16 18:36:42 +0600

    receive onSuccess

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { IKUpload } from "imagekitio-next";
 import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
 
-export default function FileUpload() {
+export default function FileUpload({onSuccess}: {onSuccess: (response: IKUploadResponse) => void}) {
 
     const [uploading, setUploading] = useState(false);
     const [error, setError] = useState<string | null>(null);
