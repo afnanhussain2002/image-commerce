911e8efe4c86919bf11b860a18d9020ae01fc5ae
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 11:19:13 +0600

    connect to the DB

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,8 +1,10 @@
+import { connectToDatabase } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, props:{params: Promise<{id: string}>}) {
     try {
         const {id} = await props.params;
+        await connectToDatabase();
         
     } catch (error) {
         console.error("Error fetching single product:", error);
