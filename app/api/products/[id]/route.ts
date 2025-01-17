3c0802cbf7c1e2576514ede987dd61625c77b77c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 11:16:47 +0600

    add a try catch block

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, props:{params: Promise<{id: string}>}) {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 
 }
