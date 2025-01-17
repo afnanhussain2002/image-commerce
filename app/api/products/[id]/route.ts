58ce8b8df728f78551ddb45071ad0baf48b621ca
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 11:19:38 +0600

    find the product on DB

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,11 +1,13 @@
 import { connectToDatabase } from "@/lib/db";
+import Product from "@/models/Product";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, props:{params: Promise<{id: string}>}) {
     try {
         const {id} = await props.params;
         await connectToDatabase();
-        
+
+        const product = await Product.findById(id).lean();
     } catch (error) {
         console.error("Error fetching single product:", error);
         return NextResponse.json({ error: "Failed to fetch single product" }, { status: 500 });
