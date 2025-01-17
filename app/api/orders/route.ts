622c76212b0b72ad2118ea9075767bbc97625536
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 12:11:01 +0600

    define a try catch block

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -3,5 +3,9 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST(request: Request) {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
