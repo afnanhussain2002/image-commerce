c06d8a1414ea98346d104226ebd09b3aa3406c01
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 12:11:36 +0600

    get the session for verify the user

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -4,7 +4,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST(request: Request) {
     try {
-        
+        const session = await getServerSession(authOptions);
     } catch (error) {
         
     }
