f4823f89f296cb3fccab82c7c0dbac9ffb7f1eb2
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-17 11:17:41 +0600

    handle the catch part

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,10 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, props:{params: Promise<{id: string}>}) {
     try {
         
     } catch (error) {
-        
+        console.error("Error fetching single product:", error);
+        return NextResponse.json({ error: "Failed to fetch single product" }, { status: 500 });
     }
 
 }
