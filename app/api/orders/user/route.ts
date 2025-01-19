bec8a1781d335524aa8123592c2cdbd783bf1c0c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-19 20:26:52 +0600

    show un auth if no session found

--- app/api/orders/user/route.ts
+++ app/api/orders/user/route.ts
@@ -1,9 +1,14 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
         const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
     } catch (error) {
         console.error("orders get error", error);
     }
