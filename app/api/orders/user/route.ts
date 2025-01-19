96138a71908d9a5125748081ceb5b386874e7cb4
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-19 20:26:08 +0600

    get the session data

--- app/api/orders/user/route.ts
+++ app/api/orders/user/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
     try {
-        
+        const session = await getServerSession(authOptions);
     } catch (error) {
         console.error("orders get error", error);
     }
