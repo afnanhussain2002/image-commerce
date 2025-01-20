d1ed55a51931fad3b9f2e59defa1cf8848275112
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-20 10:15:51 +0600

    get the body

--- app/api/webhook/razorpay/route.ts
+++ app/api/webhook/razorpay/route.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
     try {
+        const body = await request.text();
         
     } catch (error) {
         
