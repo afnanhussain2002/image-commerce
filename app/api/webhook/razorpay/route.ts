a2db159f826bebbfa6ae1ecf2c9f6853f78d43a7
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-20 10:16:34 +0600

    write a signature in a variable

--- app/api/webhook/razorpay/route.ts
+++ app/api/webhook/razorpay/route.ts
@@ -3,6 +3,7 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
     try {
         const body = await request.text();
+        const signature = request.headers.get('x-razorpay-signature');
         
     } catch (error) {
         
